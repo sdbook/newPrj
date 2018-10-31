4b8531a4f3e896052ff0b4a360ce8e05875dc7a7
R268-PC\R268 <sdbook@gmail.com>
2018-10-31 10:09:12 +0800

    added: add2Cart function

--- shop/showCart.php
+++ shop/showCart.php
@@ -1,7 +1,8 @@
 ﻿<?php
-require("prdModel.php");
+require("cartModel.php");
+require("userModel.php");
 //checkLogin();
-$result=getPrdList();
+$result=showCart(getCurrentUser());
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
